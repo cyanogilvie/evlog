4524dbc2489f056fecc40346be9c735aadb52e66
Cyan Ogilvie <cyan@tirion.(none)>
2011-05-28 13:15:14 +0200

    Implemented offloading evlog sending to a thread

--- client/evlog.tcl
+++ client/evlog.tcl
@@ -5,6 +5,7 @@ namespace eval ::evlog {
 	namespace ensemble create
 
 	variable con
+	variable tid
 
 	proc connect {sourcename {uri uds:///tmp/evlog.socket}} { #<<<
 		variable con
@@ -19,6 +20,7 @@ namespace eval ::evlog {
 		if {$uri eq ""} return
 
 		set con	[netdgram::connect_uri $uri]
+		proc [info object namespace $con]::log args {}
 		$con send [encoding convertto utf-8 \
 				[list init $sourcename [clock microseconds]]]
 
@@ -31,8 +33,43 @@ namespace eval ::evlog {
 	}
 
 	#>>>
+	proc connect_thread {sourcename {uri uds:///tmp/evlog.socket}} { #<<<
+		variable tid
+		if {[info exists tid]} {
+			thread::release $tid
+			unset tid
+		}
+		if {$uri eq ""} return
+		package require Thread 2.6.6
+		set tid	[thread::create -preserved [string map [list \
+				%tm_path%		[tcl::tm::path list] \
+				%auto_path%		[list $::auto_path] \
+				%uri%			[list $uri] \
+				%sourcename%	[list $sourcename] \
+		] {
+			tcl::tm::path add %tm_path%
+			set auto_path	%auto_path%
+
+			package require netdgram
+
+			set con	[netdgram::connect_uri %uri%]
+			proc [info object namespace $con]::log args {}
+			proc send packet [format {
+				%s send [encoding convertto utf-8 $packet]
+			} [list $con]]
+			send [list init %sourcename% [clock microseconds]]
+
+			thread::wait
+		}]]
+		proc event {type {details ""}} [format {
+			thread::send -async %s [list send [list ev [clock microseconds] \
+					$type [uplevel 1 [list subst $details]]]]
+		} [list $tid]]
+	}
+
+	#>>>
 
 	# Defined like this it's a fairly inexpensive nop
-	proc event {args} {}
+	proc event args {}
 }
 
